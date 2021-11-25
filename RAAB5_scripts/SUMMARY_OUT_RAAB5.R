ab80923c9c993761bbeb88aed92c7d2991e00291
Robert Butcher <icrurbut@LAPT2415.ads.lshtm.ac.uk>
2021-11-25 13:55:07 +0000

    25 Nov 21 RB

--- RAAB5_scripts/SUMMARY_OUT_RAAB5.R
+++ RAAB5_scripts/SUMMARY_OUT_RAAB5.R
@@ -1,6 +1,6 @@
-#RAAB6
+#RAAB5
 
-prebigboi1<-dcast(melt(sum3[,c("vi.level","female.adj.pct","male.adj.pct","total.adj.pct","extrapolated.female.n","extrapolated.male.n","extrapolated.total.n")], id.var="vi.level"), 1~variable+vi.level)
+prebigboi1<-dcast(melt(sum3[,c("vi.level","female.adj.pct","female.adj.pct.lci","female.adj.pct.uci","male.adj.pct","male.adj.pct.lci","male.adj.pct.uci","total.adj.pct","total.adj.pct.lci","total.adj.pct.uci","extrapolated.female.n","extrapolated.male.n","extrapolated.total.n")], id.var="vi.level"), 1~variable+vi.level)
 
 prev14_360<-prev14[prev14$denom.thresh==360,]
 prev14_660<-prev14[prev14$denom.thresh==660,]
@@ -13,7 +13,9 @@ names(prebigboi3)<-paste0(names(prebigboi3),"_operable_thresh_660")
 prebigboi4<-dcast(melt(prev14_618[,2:20], id.var="num.thresh"), 1~variable+num.thresh)
 names(prebigboi4)<-paste0(names(prebigboi4),"_operable_thresh_618")
 
-prebigboi5<-dcast(melt(asa7[,c("out.names","male.adj.pct","female.adj.pct","total.adj.pct","extrapolated.male.n","extrapolated.female.n","extrapolated.total.n")], id.var="out.names"), 1~variable+out.names)
+prebigboi5<-dcast(melt(asa7[,c("out.names","male.adj.pct","male.adj.pct.lci","male.adj.pct.uci","female.adj.pct","female.adj.pct.lci","female.adj.pct.uci","total.adj.pct","total.adj.pct.lci","total.adj.pct.uci","extrapolated.male.n","extrapolated.female.n","extrapolated.total.n")], id.var="out.names"), 1~variable+out.names)
+names(prebigboi5)<-gsub("\\ <","_",names(prebigboi5))
+names(prebigboi5)<-gsub("/","",names(prebigboi5))
 
 causes<-sum6[1:13,]
 causes<-causes[order(causes$blind.pct,decreasing=T),]
