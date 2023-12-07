735a2062c3bae83360465421b017e93d1cf2c70b
Robert Butcher <bertie.butcher@gmail.com>
2023-12-07 01:33:48 +0000

    singlesurvey update

--- rmd_wrapper_single_survey.R
+++ rmd_wrapper_single_survey.R
@@ -45,6 +45,11 @@ unlink(here("RAAB6_scripts", "*.log"))
 checker7<-read.csv(here("data", "surveys.csv"))
 DR_check<-checker7[checker7$raab_id==ID,c('raab_id','dr_diabetes_blood_consent')]
 WQ_check<-checker7[checker7$raab_id==ID,c('raab_id',"wg_difficulty_seeing","wg_difficulty_hearing","wg_difficulty_memory","wg_difficulty_mobility","wg_difficulty_selfcare","wg_difficulty_communication")]
+if(exists('binocular_near_corrected_result',where=checker7)){
+  NV_check<-checker7[checker7$raab_id==ID,c('raab_id','binocular_near_corrected_result')]
+}else{
+  NV_check<-data.frame(raab_id=rep(ID, nrow(checker7)), binocular_near_corrected_result=NA)}
+
 remove(checker7)
 
 render(here("RAAB7_scripts","RAAB7_reporter.Rmd"), output_file = here("outputs", paste0(ID,"_report")), output_dir = here("outputs", ID, "summary"))
