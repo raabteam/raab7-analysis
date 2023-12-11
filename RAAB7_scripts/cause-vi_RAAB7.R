8754b4e06d1b20085b7a5b9b6dddfec25525fa45
ianmccormick <ianjgmccormick@gmail.com>
2023-12-11 10:00:40 +0000

    111223 IM adds pterygium to causes

--- RAAB7_scripts/cause-vi_RAAB7.R
+++ RAAB7_scripts/cause-vi_RAAB7.R
@@ -39,12 +39,12 @@ for(i in seq_along(raab.cause))
 }
 
 sum6[nrow(sum6)+1,2:9]<-colSums(sum6[,2:9])
-sum6$principal.cause[15] <- "Total"
+sum6$principal.cause[16] <- "Total"
 
 sum6$cause_group_1[sum6$principal.cause=="poor_vision_cause_uncorrected_refractive_error" | sum6$principal.cause=="poor_vision_cause_aphakia_uncorrected" | sum6$principal.cause== "poor_vision_cause_cataract_untreated"]<-"A. Treatable (1, 2, 3)"
-sum6$cause_group_1[sum6$principal.cause=="poor_vision_cause_trachomatous_corneal_opacity" | sum6$principal.cause=="poor_vision_cause_other_corneal_opacity" | sum6$principal.cause=="poor_vision_cause_phthisis" | sum6$principal.cause=="poor_vision_cause_onchocerciasis"]<-"B. Preventable (PHC/PEC services) (5, 6, 7, 8)"
-sum6$cause_group_1[sum6$principal.cause=="poor_vision_cause_cataract_surgical_complications" | sum6$principal.cause=="poor_vision_cause_glaucoma" | sum6$principal.cause=="poor_vision_cause_diabetic_retinopathy"]<-"C. Preventable (Ophthalmic services) (4, 9, 10)"
-sum6$cause_group_2[sum6$principal.cause=="poor_vision_cause_onchocerciasis" | sum6$principal.cause=="poor_vision_cause_glaucoma" | sum6$principal.cause=="poor_vision_cause_diabetic_retinopathy" | sum6$principal.cause=="poor_vision_cause_age_related_macular_degeneration" | sum6$principal.cause=="poor_vision_cause_other_posterior_segment_disease" | sum6$principal.cause=="poor_vision_cause_myopic_degeneration"]<-"E. Posterior segment disease (8, 9, 10, 11, 12, 13)"
+sum6$cause_group_1[sum6$principal.cause=="poor_vision_cause_trachomatous_corneal_opacity" | sum6$principal.cause=="poor_vision_cause_other_corneal_opacity" |  sum6$principal.cause=="poor_vision_cause_pterygium" | sum6$principal.cause=="poor_vision_cause_phthisis" | sum6$principal.cause=="poor_vision_cause_onchocerciasis"]<-"B. Preventable (PHC/PEC services) (5, 6, 7, 8, 9)"
+sum6$cause_group_1[sum6$principal.cause=="poor_vision_cause_cataract_surgical_complications" | sum6$principal.cause=="poor_vision_cause_glaucoma" | sum6$principal.cause=="poor_vision_cause_diabetic_retinopathy"]<-"C. Preventable (Ophthalmic services) (4, 10, 11)"
+sum6$cause_group_2[sum6$principal.cause=="poor_vision_cause_onchocerciasis" | sum6$principal.cause=="poor_vision_cause_glaucoma" | sum6$principal.cause=="poor_vision_cause_diabetic_retinopathy" | sum6$principal.cause=="poor_vision_cause_age_related_macular_degeneration" | sum6$principal.cause=="poor_vision_cause_other_posterior_segment_disease" | sum6$principal.cause=="poor_vision_cause_myopic_degeneration"]<-"E. Posterior segment disease (9, 10, 11, 12, 13, 14)"
 
 pcts<-grep("pct",names(sum6))
 sum6[,pcts]<-round( sum6[,pcts] * 100, 1 )
